b1dd14b8aeb17e7f6a3e3a662d481b1f152ee420
@24B11DS097 <srinivaskayala39@gmail.com>
2026-03-06 22:03:48 +0530

    Create blood_camps_tables.sql for blood camps data

---
+++ databases/blood_camps_tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE blood_camps (
+    camp_id INT AUTO_INCREMENT PRIMARY KEY,
+    camp_name VARCHAR(100),
+    location VARCHAR(100),
+    camp_date DATE,
+    camp_time VARCHAR(20),
+    organizer VARCHAR(100)
+);
