0da98d5a8ebd5314e64bd23f6656b6bef55940b6
@24B11DS097 <srinivaskayala39@gmail.com>
2026-03-06 22:01:59 +0530

    Create blood_stock_table.sql for blood stock management

---
+++ databases/blood_stock_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE blood_stock (
+    blood_group VARCHAR(5) PRIMARY KEY,
+    units_available INT
+);
+
+INSERT INTO blood_stock VALUES
+('A+',0),
+('A-',0),
+('B+',0),
+('B-',0),
+('AB+',0),
+('AB-',0),
+('O+',0),
+('O-',0);
